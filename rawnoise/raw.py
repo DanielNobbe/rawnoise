1dd27ec8c0766138565bce169eaba09fc967c294
Daniel Nobbe <daniellnobbe@gmail.com>
2024-04-26 17:47:46 +0200

    refactor rawimage class to only keep track of file path

--- rawnoise/raw.py
+++ rawnoise/raw.py
@@ -15,33 +15,43 @@ class RawImage:
     """
 
     def __init__(self, path: str) -> None:
-        self.rawpy = rawpy.imread(path)
+        self.path = path
+        with rawpy.imread(self.path) as raw:
+            self.raw_image = raw.raw_image
+        # it's a bit annoying, since rawpy needs closing,
+        # it probably keeps the file open the entire time, which isn't really
+        # necessary.
+        # would be better to just keep a reference to the file path,
+        # and load the file when necessary for postprocessing, otherwise just keep
+        # the raw array
 
     def to_array(self) -> None:
-        return self.rawpy.raw_image
+        return self.raw_image
     
     def to_tensor(self) -> None:
-        return torch.from_numpy(self.rawpy.raw_image)
+        return torch.from_numpy(self.raw_image)
     
     def update_from_array(self, array: NDArray) -> None:
         # first verify that the size of the new array is the same
         # note: array could be type annotated with dtype, not sure what that is in rawpy though
-        raw_height = self.rawpy.sizes.raw_height
-        raw_width = self.rawpy.sizes.raw_width
 
-        array_height = array.shape[0]
-        array_width = array.shape[1]
-
-        if raw_height != array_height or raw_width != array_width:
-            raise ValueError(f"New array size ({array_height}, {array_width}) doesn't match the raw image size ({raw_height}, {raw_width})")
+        if array.shape != self.raw_image.shape:
+            raise ValueError(f"New array size ({array.shape}) doesn't match the raw image size ({self.raw_image.shape})")
         
-        # assign values to raw_image array, we can't assign a new object as raw_image
-        self.rawpy.raw_image[:] = array[:]
+        # assign values to raw_image array (could also replace attr)
+        self.raw_image[:] = array[:]
 
     def update_from_tensor(self, tensor: torch.Tensor) -> None:
         array = tensor.numpy()
 
         self.update_from_array(array)
+
+    @staticmethod
+    def update_raw(raw, array: NDArray) -> None:
+        raw.raw_image[:] = array[:]
     
     def postprocess(self) -> NDArray:
-        return self.rawpy.postprocess()
+        with rawpy.imread(self.path) as raw:
+            self.update_raw(raw, self.raw_image)
+            result = raw.postprocess(demosaic_algorithm=rawpy.DemosaicAlgorithm.AHD,  use_camera_wb=False)
+        return result
