1f39095e5f1b29ba4e267715d8c220d9fd7ceed2
Daniel Nobbe <daniellnobbe@gmail.com>
2024-05-06 22:28:46 +0200

    fixes

--- rawnoise/noise/pipeline.py
+++ rawnoise/noise/pipeline.py
@@ -1,8 +1,10 @@
 import torch
 import os
 from rawnoise.raw import RawImage
+from rawnoise.noise import ReadNoise
+from copy import deepcopy
 
-class NoisePipeline:
+class NoisePipeline(torch.nn.Module):
     """
         This class defines a synthetic noise pipeline. It takes in a raw
         image (RawImage class), and adds noise to its raw array.
@@ -11,13 +13,17 @@ class NoisePipeline:
         We use PyTorch so that we can implement the postprocessing pipeline
         in PyTorch at a later stage too. 
     """
-    def __init__(self, read_noise_fn) -> None:
-        self.read_noise_fn = read_noise_fn
+    def __init__(self, read_noise) -> None:
+        super().__init__()
+        self.read_noise = read_noise
 
     def apply_noise(self, tensor: torch.Tensor) -> torch.Tensor:
-        return self.read_noise_fn(tensor)
+        return self.read_noise(tensor)
 
-    def __call__(self, raw: RawImage) -> RawImage:
+    def __call__(self, raw: RawImage, copy: bool = False) -> RawImage:
+        if copy:
+            # if the original object can't be modified, set this to true
+            raw = deepcopy(raw)
         tensor = raw.to_tensor()
 
         tensor = self.apply_noise(tensor)
