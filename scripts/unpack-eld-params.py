3ff318e7beffd8a16ec7537052665bf62533955d
Daniel Nobbe <daniellnobbe@gmail.com>
2024-05-24 17:41:27 +0200

    notes in unpack ELD script

--- scripts/unpack-eld-params.py
+++ scripts/unpack-eld-params.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import json
 
 def main():
 
@@ -75,7 +76,59 @@ def main():
     sigma = g_scale_bias + g_scale_slope * 1.0
 
     print(sigma)
+    breakpoint()
 
+    """Tukey lambda has three params:
+    - shape (lambda)
+    - scale (sigma)
+    - colour bias (mu)
+
+    Now, we have for G_scale three values:
+    - sigma
+    - bias
+    - slope
+    --> these are probably used to sample a value for the scale of G, analogously to the Guassian scale
+
+    Outside of the `Profile-1`, there is a `G_shape` and `color_bias`, these probably refer to the shape factor (lambda) and the mu.
+    Why does G_shape have 18 values?
+    And why is the shape of color_bias 18x4?
+
+    Colour bias is four colour pixels:
+    R, G1, B, G2
+
+    That explains the shape of colour_bias
+
+    They could not find a statistical model describing the lambda and mu values,
+    so these should just be sampled from the empirical distribution, which
+    apparently consists of 18 values.
+    Note that for most parameters, they depend on K, which is the selected system gain, which correlates to ISO. As such, we can find a value for
+    G_scale based on K, but not G_shape. It's odd, since the average G_shape is around 0, and the same goes for the colour_bias. 
+    Note that this is for the SAME camera, under different 
+    gain values.. So should we not select the values with gain closest?
+    Seems like they didn't leave every stone unturned -- why is the colour
+    bias and shape different every time?
+    The scale decides the magnitude of the noise, in addition to the gain, so obviously the _amount_ of noise increases as the gain increases.
+    But the distribution of the noise itself is different every time..
+
+    For now, calculate what the scale should be, and we just randomly pick one of the bias/shape pairs:
+     [ 1.1709546 ,  1.0815442 ,  1.1473132 ,  1.0724843 ], -0.1428
+
+    They give a method for sampling the scale value, but let's
+    keep it constant for now, just taking the mean they get there
+
+    log(sigma) = sigma_slope*log(K) + sigma_bias
+    we set log(K) to 1, since that came out of the uniform distr above
+
+    """
+
+    # G_scale entries are for Tukey Lambda?
+    G_scale_sigma = params['Profile-1']['G_scale']['sigma']
+    G_scale_bias = params['Profile-1']['G_scale']['bias']
+    G_scale_slope = params['Profile-1']['G_scale']['slope']
+
+    sigma = G_scale_bias + G_scale_slope * 1.0
+    
+    
 
 if __name__ == '__main__':
     main()
