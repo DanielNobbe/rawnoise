e718da04c4c66b3ce703ffc510190438f7d493db
Daniel Nobbe <daniellnobbe@gmail.com>
2024-05-27 20:16:17 +0200

    add tukey read noise to pipeline script

--- scripts/noise-pipeline.py
+++ scripts/noise-pipeline.py
@@ -1,4 +1,4 @@
-from rawnoise.noise import NoisePipeline, ReadNoise
+from rawnoise.noise import NoisePipeline, TukeyReadNoise, GaussianReadNoise
 from rawnoise.raw import RawImage
 
 def main():
@@ -8,11 +8,12 @@ def main():
 
     uint14_max = 16384  # values are 14bit in raw? TODO: CHeck
     
-    sigma = 1.7 / uint14_max
+    sigma = 2.8 / uint14_max
     ratio = 100
 
     pipeline = NoisePipeline(
-        read_noise=ReadNoise(\
+        read_noise=TukeyReadNoise(
+            lamda=-0.1428,
             sigma=sigma,
             ratio=ratio,
             K = 0.1  # from ELD it's ~0.09
@@ -21,11 +22,25 @@ def main():
 
     # right, in the sampling code in ELD they take the log of this value
 
-    new_raw = pipeline(raw)
+    tukey_raw = pipeline(raw, copy=True)
 
     # raw.update_from_tensor(new_tensor)
 
-    raw.postprocess(save='outputs/DSC00866_noised.tiff')
+    tukey_raw.postprocess(save='outputs/DSC00866_tukey.tiff')
+
+    sigma = 5.2 / uint14_max  # this gives about the same as 2.8 for tukey
+    # guassian default is 1.7
+    gauss_pipeline = NoisePipeline(
+        read_noise=GaussianReadNoise(
+            sigma=sigma,
+            ratio=ratio,
+            K = 0.1  # from ELD it's ~0.09
+        )
+    )
+
+    gauss_raw = gauss_pipeline(raw, copy=True)
+
+    gauss_raw.postprocess(save='outputs/DSC00866_gauss.tiff')
 
 
 
