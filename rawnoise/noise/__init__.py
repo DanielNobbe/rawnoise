2d063195edc823d04ee47029ab1f20cbd8ddfc57
Daniel Nobbe <daniellnobbe@gmail.com>
2024-05-30 20:32:40 +0200

    add functions to process params json into dataclasses

--- rawnoise/noise/__init__.py
+++ rawnoise/noise/__init__.py
@@ -1,2 +1,3 @@
 from .read_noise import GaussianReadNoise, TukeyReadNoise
 from .pipeline import NoisePipeline
+from .params import load_params_from_json
---
+++ rawnoise/noise/params.py
@@ -0,0 +1,51 @@
+import os
+import json
+from dataclasses import dataclass
+from collections import namedtuple
+
+@dataclass
+class GainParams:
+    Kmin: float
+    Kmax: float
+
+
+@dataclass
+class ScaleParams:
+    slope: float
+    sigma: float
+    bias: float
+
+
+@dataclass
+class TukeyParams:
+    scale: ScaleParams
+    shape: list[float]
+    color_bias: list[tuple[float, float, float, float]]
+
+
+
+def load_gain_params(params: dict) -> GainParams:
+    return GainParams(Kmin=params['Kmin'], Kmax=params['Kmax'])
+
+def load_tukey_params(params: dict) -> TukeyParams:
+    return TukeyParams(
+        ScaleParams(
+            slope=params['G_scale']['slope'],
+            sigma=params['G_scale']['sigma'],
+            slope=params['G_scale']['bias']
+        ),
+        shape=params['G_shape'],
+        color_bias=params['color_bias']
+    )
+
+def load_params_from_json(file: str | os.PathLike):
+    with open(file, 'r') as jf:
+        params = json.load(jf)
+
+    tukey_params = load_tukey_params(params)
+    gain_params = load_gain_params(params)
+
+    return {
+        'tukey': tukey_params,
+        'gain': gain_params
+    }
