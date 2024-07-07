4c3345ac561eb66679a07d2a20e0ea74ca61c52b
Daniel Nobbe <daniellnobbe@gmail.com>
2024-07-07 14:01:12 +0200

    add tukeysampler to module

--- rawnoise/noise/__init__.py
+++ rawnoise/noise/__init__.py
@@ -1,4 +1,4 @@
 from .params import load_params_from_json
 from .pipeline import NoisePipeline
-from .read_noise import GaussianReadNoise, TukeyReadNoise
+from .read_noise import GaussianReadNoise, TukeyReadNoise, TukeySampler
 from .shot_noise import GainSampler, ShotNoise
