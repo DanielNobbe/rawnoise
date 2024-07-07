dfd88f9895a33b63899b5150efcc63b5fb8195bf
Daniel Nobbe <daniellnobbe@gmail.com>
2024-07-07 14:01:48 +0200

    noise pipeline with shot noise and tukey lamda read noise

--- rawnoise/noise/pipeline.py
+++ rawnoise/noise/pipeline.py
@@ -2,8 +2,9 @@ import torch
 import os
 from rawnoise.raw import RawImage
 from copy import deepcopy
-from .params import GainParams
+from .params import GainParams, TukeyParams
 from .shot_noise import GainSampler, ShotNoise
+from .read_noise import TukeyReadNoise, TukeySampler
 
 class NoisePipeline(torch.nn.Module):
     """
@@ -17,19 +18,26 @@ class NoisePipeline(torch.nn.Module):
         Here we should sample the K parameter, which is central to
         multiple types of noise.
     """
-    def __init__(self, read_noise=None, shot_noise: ShotNoise = None, gain_sampler: GainSampler = None) -> None:
+    def __init__(self, read_noise=None, shot_noise: ShotNoise = None, gain_sampler: GainSampler = None, tukey_sampler: TukeySampler = None) -> None:
         super().__init__()
         self.read_noise = read_noise
         self.shot_noise: ShotNoise = shot_noise
         self.gain_sampler: GainSampler = gain_sampler
+        self.tukey_sampler: TukeySampler = tukey_sampler
 
     def apply_noise(self, tensor: torch.Tensor, ratio: float = 1.0) -> torch.Tensor:
         gain = self.gain_sampler.sample()
-        # for testing shot noise
-        return self.shot_noise(tensor, K=gain, ratio=ratio)
+        
+        # shot noise is a transformation applied to tensor
+        shot_noise = self.shot_noise(tensor, K=gain, ratio=ratio)
 
+        sigma, lamda = self.tukey_sampler.sample(gain)
 
-        # return self.read_noise(tensor)
+        # read noise is also a transformation on the tensor
+        # its additivity is handled internally
+        read_noise = self.read_noise(shot_noise, sigma=sigma, lamda=lamda)
+
+        return read_noise
 
     def __call__(self, raw: RawImage, copy: bool = False) -> RawImage:
         if copy:
